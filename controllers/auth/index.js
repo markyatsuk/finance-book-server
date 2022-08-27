8a0a666f007b31e87dd473074b03b4a0bd5a5ace
Alexander Kostin <asurv2@gmail.com>
2022-08-27 21:08:10 +0300

    add verify

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,10 +1,14 @@
 const register = require("./register");
+const verifyEmail = require("./verifyEmail");
+const getCurrent = require("./getCurrent");
 const login = require("./login");
 const logout = require("./logout");
 
 
 module.exports = {
     register,
+    verifyEmail,
     login,
     logout,
+    getCurrent,
 };
