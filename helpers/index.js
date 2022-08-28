2b14fad7b5d41b49fdec61dcc4c76b9cba3a134c
eduard-ops <epustynnik@gmail.com>
2022-08-28 18:34:26 +0300

    update helpers

--- helpers/index.js
+++ helpers/index.js
@@ -1,7 +1,9 @@
 const createError = require("./createError");
 const ctrlWrapper = require("./ctrlWrapper");
+const validation = require("./validation");
 
 module.exports = {
-    createError,
-    ctrlWrapper,
+  createError,
+  ctrlWrapper,
+  validation,
 };
