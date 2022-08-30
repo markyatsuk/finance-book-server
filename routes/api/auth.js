1a61ae906817cfe6c2e71e58391e0282d0403c92
Alexander Kostin <asurv2@gmail.com>
2022-08-31 00:16:00 +0300

    del comments

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const { basedir } = global;
 
-const  ctrl = require(`${basedir}/controllers/auth`);
+const ctrl = require(`${basedir}/controllers/auth`);
 
 const { ctrlWrapper } = require(`${basedir}/helpers`);
 
@@ -12,19 +12,13 @@ const router = express.Router();
 
 router.post("/register", ctrlWrapper(ctrl.register));
 
-// router.get("/verify", ctrlWrapper(ctrl.verifyEmail));
-
-// router.get("/verify/:verificationToken", ctrlWrapper(ctrl.verifyEmail));
-
-// router.post("/verify", ctrlWrapper(ctrl.resendVerifyEmail));
-
 router.post("/login", ctrlWrapper(ctrl.login));
 
 router.get("/current", auth, ctrlWrapper(ctrl.getCurrent));
 
-router.get('/google', ctrlWrapper(ctrl.googleAuth));
+router.get("/google", ctrlWrapper(ctrl.googleAuth));
 
-router.get('/google-redirect', ctrlWrapper(ctrl.googleRedirect));
+router.get("/google-redirect", ctrlWrapper(ctrl.googleRedirect));
 
 router.get("/logout", auth, ctrlWrapper(ctrl.logout));
 
