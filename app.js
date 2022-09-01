142827388e2300d077e73e165a37a746b5dce939
YaroslavPlaksii91 <yarosyk@ukr.net>
2022-09-01 22:26:48 +0300

    change swagger url

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/api/auth", authRouter);
 
